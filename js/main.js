104dd143bb6cb99b8b586135a609d0eef240011d
Fulminant <fulminantd@gmail.com>
2015-04-02 17:40:09 +0300

    add image blur

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-$(document).ready(function(){
+$(window).load(function(){
+
+//Slider Activeta
 var swiper = new Swiper('.swiper-container', {
         pagination: '.swiper-pagination',
         paginationClickable: '.swiper-pagination',
@@ -6,4 +8,12 @@ var swiper = new Swiper('.swiper-container', {
         effect: 'fade',
         autoplayDisableOnInteraction: false
     });
+
+//Blured Background
+
+  if ($('.our-skills').has('blurimage')) {
+    $('.our-skills').append("<canvas id=\"blured-image\"></canvas>");
+    stackBoxBlurImage('blurimage','blured-image',6, false, 1);
+  }
+
 });
